37a2b0bb31c7878c52ec163ddbc5d45c159d6bb5
Sebastián <alejandro.jimenez@epn.edu.ec>
2023-12-19 19:53:28 -0500

    feat: borrar metodo inutil

--- src/main/java/com/example/tarea3/Comprobante.java
+++ src/main/java/com/example/tarea3/Comprobante.java
@@ -7,12 +7,6 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class Comprobante {
-    public void generarComprobante(JuguetesEntity juguete, Model model) {
-        model.addAttribute("IDJuguete", juguete.getId());
-        model.addAttribute("Precio", juguete.getPrecio());
-        model.addAttribute("Cantidad", juguete.getCantidad());
-    }
-
     public void generarComprobanteVenta(int nuevaCantidad, BigDecimal precio, Model model) {
         model.addAttribute("nuevaCantidad", nuevaCantidad);
         model.addAttribute("precioUnitario", precio);
